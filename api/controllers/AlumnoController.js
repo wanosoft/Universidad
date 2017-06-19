a12bb2cca7990b4a62b3ea97eee0eb5bcbfaf9f9
UPAM <upam@iMac-de-UPAM.local>
2017-06-19 14:38:09 -0500

    Show view por findOne

--- api/controllers/AlumnoController.js
+++ api/controllers/AlumnoController.js
@@ -8,25 +8,39 @@
 module.exports = {
 
 	new:function(req,res){
-		console.log("Entraste a New");
+		console.log('Entraste a New');
 		res.view();
 	},
 
-	create:function(req,res,next){
+	create:function(req,res){
 			var alumnoObj = {
+				matricula: req.param('idmatricula'),
 				nombre: req.param('nombre'),
-				apellido_paterno:req.param('ap');
-				apellido_materno:req.param('ap');
-				edad:req.param('edad');
-				cursa.req.param('carrera');
+				apellido_paterno:req.param('ap'),
+				apellido_materno:req.param('am'),
+				edad:req.param('edad'),
+				cursa:req.param('carrera')
 			}
+
+			// Alumno es el modelo
+			Alumno.create(alumnoObj, function(err, alumno){
+				if(err){
+					return res.redirect('alumno/new');
+				}
+				sails.log('alumno %s%', alumno);
+				res.redirect('alumno/show/?matricula='+alumno.matricula);
+			})
 	},
 
-	// Alumno es el modelo
-	Alumno.create(alumnoObj, function(err,alumno){
-		if(err){
-			return res.redirect('alumno/new');
-		}
-		return res.redirect('alumno/new');
-	})
+	show:function(req,res,next){
+		sails.log('Entraste a show');
+		sails.log('Matricula: --> '+req.param('matricula'));
+
+		Alumno.findOne({matricula:req.param('matricula')},
+				function(err,alumno){
+					if(err){
+						return next(err);
+					} res.view({alumno:alumno});
+				});
+	}
 };
