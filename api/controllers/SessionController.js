14f1467f0a71122c4b8b31fdf18e60af9c8e3b3f
David Arellano <wanosoft@gmail.com>
2017-07-03 14:42:13 -0500

    Validación

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -5,7 +5,55 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+var bcrypt=require('bcrypt');
 
+module.exports = {
+	new:function(req, res){
+		res.view();
+	},
+
+	create:function(req, res, next) {
+		console.log("inicio logeo");
+		var username=req.param('nombre');
+		var password=req.param('psw');
+
+		if (!username || !password) {
+			var errorUser = [{message:'El usuario y/o contraseña no son válidos'}]
+
+			req.session.flash = {
+				err:errorUser
+			}
+			return res.redirect('sessions/new');
+		}
+
+		Alumno.findOne({matricula:username}, function userfounded(err,user){
+			if (err) {
+				req.session.flash = {err:err}
+				return res.redirect('session/new');
+			}
+
+			if (!user) {
+				var noUserError = [{message:'No se encuentra al usuario'}]
+				req.session.flash = {
+					err:noUserError
+				}
+				return res.redirect('session/new');
+			}
+			bcrypt.compare(password, user.encryptedPassword,function passwordMatch(err, valid){
+				if (err) {
+					req.session.flash = {err:err}
+					return res.redirect('session/new');
+				}
+
+				if (!valid) {
+					var noUserError = [{message: 'No match password'}]
+					req.session.flash = {
+						err:noUserError
+					}
+					return res.redirect('session/new')
+				}
+				return res.redirect('/alumno/show/?matricula='+user.matricula);
+			});
+		});
+	}
+};
